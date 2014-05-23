e62bbd1ddfe65cc03077bd37fd2d801a1ae03b37
James <topskill@gmail.com>
2014-05-23 16:49:23 +0800

    为 elpa 增加 marmalade 源和 melpa 源的声明。

--- .emacs.d/elisp/elpa.el
+++ .emacs.d/elisp/elpa.el
@@ -2,5 +2,9 @@
 
 (require 'cl)
 (require 'package)
-(add-to-list 'package-archives '("marmalade" "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+  '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
 (package-initialize)
