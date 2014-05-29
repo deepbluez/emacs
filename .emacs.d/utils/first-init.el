e8ac7c98da0ba5a315f0b3dca6ac46495f0f4ed2
James Wang <topskill@gmail.com>
2014-05-29 20:17:14 +0800

    增加初始化脚本，用于在新主机上部署配置。

---
+++ .emacs.d/utils/first-init.el
@@ -0,0 +1,9 @@
+;; -*- encoding: utf-8 -*-
+
+;; 首次安装后，执行此脚本以安装相关插件
+
+(package-install 'tabbar)
+(package-install 'tabbar-ruler)
+(package-install 'ecb)
+(package-install 'pymacs)
+(package-install 'yasnippet)
