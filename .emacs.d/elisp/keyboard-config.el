8a445a9c6b8bfa5a563c163b99f789df48fbe2b3
James <topskill@gmail.com>
2014-05-29 22:50:07 +0800

    增加执行选区或文件的快捷键。

--- .emacs.d/elisp/keyboard-config.el
+++ .emacs.d/elisp/keyboard-config.el
@@ -10,3 +10,19 @@
 ;; 上一个/下一个标签页(默认是 C-c C-left 和 C-c C-right 太难按了)
 (global-set-key (kbd "M-N") 'tabbar-forward)
 (global-set-key (kbd "M-P") 'tabbar-backward)
+
+;; 在 lisp 模式中，将F5绑定为执行当前缓冲或选区（若选中了文本）
+(defun eval-buffer-or-region ()
+   "若存在选区则运行选区，否则运行当前文件"
+   (interactive)
+   (if (region-active-p)
+       (eval-region)
+     (eval-buffer)
+   ))
+(dolist (hook (list
+	       'emacs-lisp-mode-hook
+	       'lisp-interactive-mode-hook))
+  (add-hook hook
+	    (lambda ()
+	      (local-set-key (kbd "<f5>") 'eval-buffer-or-region)
+	      )))
