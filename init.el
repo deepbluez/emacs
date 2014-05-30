842797adb261f7fff1a3237cc30e094cf8f0c4c2
james <topskill@gmail.com>
2014-05-30 15:51:49 +0800

    将 Emacs 自动生成的配置文件移动到 .emacs 中

--- init.el
+++ init.el
@@ -10,18 +10,6 @@
 ;; 关掉初始画面
 (setq inhibit-startup-message t)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
- '(custom-enabled-themes (quote (obsidian)))
- '(custom-safe-themes (quote ("17a4e8ceb0320f07f4087c85ad4a452fc82d8b7b70d16eda8e3f4df138223399" "d2b1a25a5e2ed8815fe07a7ebf1b27e7095966bd0eb5afb8d0d1e85ef4809998" "" default)))
- '(safe-local-variable-values (quote ((encoding . utf-8))))
- '(tool-bar-mode nil))
-
-
 
 ;; ----------------------------------------------
 ;; 下面是按照不同功能分开加载的配置项
