2e9d4bce5384eb6796f501b02fc4b7dc50e72ea5
James Wang <topskill@gmail.com>
2014-06-03 22:53:47 +0800

    设置 markdown 转换器的命令行。

--- elisp/elpa.el
+++ elisp/elpa.el
@@ -15,3 +15,7 @@
 
 ;; 启用 TabBar
 (tabbar-mode t)
+
+;; 设置 markdown 的默认转换器。要求系统中安装 pandoc
+;; 设置语法高亮模式为 pygments，与 bitbucket.org 兼容
+(setq markdown-command "pandoc -f markdown -t html -s --highlight-style pygments")
