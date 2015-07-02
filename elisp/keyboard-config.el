d3bb14e779b53bb0a988101386ef1a6ef6f3dd48
James Wang <topskill@gmail.com>
2015-07-03 01:36:45 +0800

    在字符模式下， C-z 保持其原有功能

--- elisp/keyboard-config.el
+++ elisp/keyboard-config.el
@@ -3,8 +3,8 @@
 ;; 按键绑定设置，所有与默认值不同的按键设置均在此文件中统一配置。
 
 ;; 将 C-z 绑定为前缀按键，在 Windows 中不需要自动最小化
-(define-prefix-command 'ctl-z-map)
-(global-set-key (kbd "C-z") 'ctl-z-map)
+(when window-system (define-prefix-command 'ctl-z-map))
+(when window-system (global-set-key (kbd "C-z") 'ctl-z-map))
 
 ;; tabbar 按键绑定
 ;; 上一个/下一个标签页(默认是 C-c C-left 和 C-c C-right 太难按了)
