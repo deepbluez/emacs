249e89de74898a8c14017771181b72e8b79e1740
James <topskill@gmail.com>
2016-11-05 19:09:15 +0800

    手动加载最新的 org-mode 插件

--- elisp/plugins.el
+++ elisp/plugins.el
@@ -2,4 +2,13 @@
 
 ;; 手工插件包管理器
 
-(require 'ibuffer)
+;; (require 'ibuffer)
+
+;; git-emacs
+(add-to-list 'load-path "~/.emacs.d/plugins/git-emacs")
+(require 'git-emacs)
+
+;; org-mode
+(setq load-path (cons "~/.emacs.d/plugins/org-mode/lisp" load-path))
+(setq load-path (cons "~/.emscs.d/plugins/org-mode/contrib/lisp" load-path))
+(require 'org-loaddefs)
