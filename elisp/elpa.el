110a9cc9e93a8f933626fe140170f8660d0408ed
James <topskill@gmail.com>
2016-11-05 22:45:39 +0800

    激活 recentf 插件，添加文件历史功能

--- elisp/elpa.el
+++ elisp/elpa.el
@@ -2,10 +2,15 @@
 
 (require 'cl)
 (require 'package)
-(add-to-list 'package-archives
-	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(add-to-list 'package-archives
-  '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+(setq package-archives
+      '(
+	("gnu"   . "http://elpa.zilongshanren.com/gnu/")
+	("melpa" . "http://elpa.zilongshanren.com/melpa/")
+	("melpa-stable" . "http://elpa.zilongshanren.com/melpa-stable/")
+	("marmalade" . "http://elpa.zilongshanren.com/marmalade/")
+	("org" . "http://elpa.zilongshanren.com/org/")
+	))
 
 (package-initialize)
 
@@ -19,3 +24,12 @@
 ;; 设置 markdown 的默认转换器。要求系统中安装 pandoc
 ;; 设置语法高亮模式为 pygments，与 bitbucket.org 兼容
 (setq markdown-command "pandoc -f markdown -t html -s --highlight-style pygments")
+
+;; 启动 recent-file 插件
+(require 'recentf)
+(load "elpa-recentf-extras")
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+;; 绑定 C-x C-r 到打开历史记录清单
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+(run-at-time nil (* 5 60) 'recentf-save-list)
